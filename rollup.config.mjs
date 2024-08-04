5c8f34c254301856c94cc7bfe7013296e33b1164
NSMichelJ <nunezsalmeronm@gmail.com>
2024-08-04 14:18:36 -0400

    Prepare the project for production

---
+++ rollup.config.mjs
@@ -0,0 +1,49 @@
+// rollup.config.mjs
+
+import fs from "fs";
+
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import { babel } from "@rollup/plugin-babel";
+import dts from "rollup-plugin-dts";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import terser from "@rollup/plugin-terser";
+
+const config = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: config.main,
+        format: "cjs",
+        sourcemap: false,
+      },
+      {
+        file: config.module,
+        format: "esm",
+        sourcemap: false,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+      }),
+      babel({
+        exclude: "node_modules/**",
+        babelHelpers: "bundled",
+      }),
+      resolve(),
+      commonjs(),
+      terser(),
+    ],
+  },
+  {
+    input: config.module,
+    output: [{ file: config.types, format: "esm" }],
+    plugins: [dts()],
+  },
+];
