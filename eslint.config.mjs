31210becfee39f7bda97305a627ee8e93b5737db
NSMichelJ <nunezsalmeronm@gmail.com>
2024-08-03 21:51:10 -0400

    Add EsLint and Prettier to the project

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+  {
+    settings: {
+      react: {
+        version: "detect",
+        defaultVersion: "18.0.0",
+      },
+    },
+  },
+  {
+    rules: {
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
